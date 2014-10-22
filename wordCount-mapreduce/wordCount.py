4f7b74d047ae026ea7f579aa4fe8abbbb777b0da
Jiada <toendex@gmail.com>
2014-10-22 17:10:34 -0500

    maintain

---
+++ wordCount-mapreduce/wordCount.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import re
+from collections import Counter
+
+#print 'Number of arguments:', len(sys.argv), 'arguments.'
+#print 'Argument List:', str(sys.argv[1:])
+cnt=Counter()
+index=int(sys.argv[1])
+outputFileNum=int(sys.argv[2])
+for fileName in sys.argv[3:]:
+    if not os.path.isfile(fileName):
+        print >> sys.stderr, 'wordcount error: file name \"%s\" not exist.' % (fileName)
+    f=open(fileName, 'r')
+    token=filter(None,re.split('\W+', f.read().decode('utf-8'), flags=re.UNICODE))
+    cnt.update(token)
+
+filePatten=os.path.dirname(sys.argv[3])+'/../output/countInter-'+str(index)+'-'
+
+outputFileList=[]
+for i in range(0,outputFileNum):
+    f=open(filePatten+i.zfill(4))
+    outputFileList.append(f)
+
+for word,num in cnt.items():
+    i=hash(word)%index
+    outstr='%d\t%s\n' % (num, word)
+    outputFileList[i].write(outstr.encode('utf-8'))
+
+for f in outputFileList:
+    f.close()
