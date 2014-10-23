4f91d5ffaef6984004c76dde1af9c73cace9f337
Ubuntu <ubuntu@ip-172-31-3-79.us-west-2.compute.internal>
2014-10-23 04:36:03 +0000

    a

--- wordCount-s3fs/wordCount.swift
+++ wordCount-s3fs/wordCount.swift
@@ -1,29 +1,26 @@
 type file;
 
-file wc_script <"wordCount.py">;
-file m_script <"merge.py">;
-
-app (file outfile) wordCount (file infile, file wc_script)
+app (file outfile) wordCount (file infile)
 {
-    python @wc_script @infile stdout=@outfile;
+    python "/s3/wordCount-s3fs/wordCount.py" @infile stdout=@outfile;
 }
 
-app (file outfile) merge (file[] infiles, file m_script)
+app (file outfile) merge (file[] infiles)
 {
-    python @m_script @filenames(infiles) stdout=@outfile;
+    python "/s3/wordCount-s3fs/merge.py" @filenames(infiles) stdout=@outfile;
 }
 
 file infile[] <filesys_mapper;pattern="split-*", location="/s3/wordCount-s3fs/input">;
-file wcfile[] <simple_mapper;prefix="wcfile",suffix=".txt", padding=4, location="/s3/wordCount-s3fs/output">; 
+file wcfile[] <simple_mapper;prefix="wcfile",suffix=".txt", padding=4, location="/s3/wordCount-s3fs/output">;
 file wclog[] <simple_mapper;prefix="wclog",suffix=".txt", padding=4, location="/s3/wordCount-s3fs/output">;
 
 tracef("test\n");
 foreach f,i in infile {
 //    tracef("%s\n",@f);
-    wcfile[i]=wordCount(f,wc_script);
+    wcfile[i]=wordCount(f);
 }
 
 file mfile <"/s3/wordCount-s3fs/output/result.txt">;
 file mlog <"/s3/wordCount-s3fs/output/mlog.txt">;
 
-mfile=merge(wcfile,m_script);
+mfile=merge(wcfile);
