a4ab7e8c044e90c08d0f17c68e158cd1ec689b8e
Jiada <toendex@gmail.com>
2014-10-23 17:34:32 -0500

    a

--- wordCount-s3fs-mapreduce/final.py
+++ wordCount-s3fs-mapreduce/final.py
@@ -9,7 +9,7 @@ from itertools import izip
 cnt=Counter()
 for fileName in sys.argv[1:]:
     if not os.path.isfile(fileName):
-        print >> sys.stderr, 'wordcount error: file name \"%s\" not exist.' % (fileName)
+        print >> sys.stderr, 'final error: file name \"%s\" not exist.' % (fileName)
     f=open(fileName, 'r')
     token=filter(None,re.split('\W+', f.read().decode('utf-8'), flags=re.UNICODE))
     cnt.update({token[i+1]:int(token[i]) for i in range(0,len(token),2)})
